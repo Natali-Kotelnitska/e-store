f730e6be96a4cf4223c6b769522031ff550bdf9a
Natali Kotelnytska <nkotelnicka@gmail.com>
2024-06-14 19:18:11 +0300

    Change controller to use api/v1

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::ProductsController < ApplicationController
+class Api::V1:ProductsController < ApplicationController
   before_action :set_product, only: %i[ show update destroy ]
 
   # GET /products
